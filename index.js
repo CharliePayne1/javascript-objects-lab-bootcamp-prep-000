4712ca384a8e5890f8e76f2b03d720929777e749
Charlie Payne <charliepayne1101@gmail.com>
2020-04-08 09:05:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
-var recipes = new Object();
+var recipes = {};
 
-function updateObjectWithKeyAndValue(object, key, value) { return Object.assign({}, object, {[key]: value })
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object, {[key]: value});
 }
-
-function destructivelyUpdateObjectWithKeyAndValue(object, key, value) { object[key] = value; return object}
-
-function deleteFromObjectByKey(object, key){
-  var newObj =  Object.assign({},object)
-  delete newObj[key]
-  return newObj
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value;
+  return object;
 }
-
-function destructivelyDeleteFromObjectByKey(object, key) { delete object[key]; return object}
+function deleteFromObjectByKey(object, key) {
+  var newObject = Object.assign({}, object);
+  delete newObject[key];
+  return newObject;
+}
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key];
+  return object;
+}
